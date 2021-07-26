7b480198e21a5887a458497953bcff29c8127ef9
Lawrence Ikhidero <43246123+lorens247@users.noreply.github.com>
2021-07-26 14:23:01 +0100

    login

---
+++ app/Controllers/Login.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+use CodeIgniter\Services;
+
+class Login extends BaseController
+{
+	public function index()
+	{
+		helper('form');
+
+		if ($this->request->getMethod() === 'post') {
+			/** @var \App\Models\Users $repository */
+			$repository = model('Users');
+			
+			$user = $repository
+				->where('login', $this->request->getPost('login'))
+				->first();
+
+			if (
+				!$user ||
+				!password_verify(
+					$this->request->getPost('password'),
+					$user['password']
+				)
+			) {
+				return redirect()
+					->to('/login')
+					->withInput()
+					->with('errors', 'Wrong password'); //key, message
+			}
+
+			$session = Services::session();
+
+			$session->set('isLoggedIn', true);
+
+			$session->set('userData', [
+				'id'	=> $user['id'],
+				'login' => $user['login']
+			]);
+
+			return redirect()->to('/products');
+		}
+		
+		return view('login/index');
+	}
+}
